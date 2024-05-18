96898e21e0b93b02b1dab338fa2b465641a6e6dd
ashera <alexis.variot@gmail.com>
2024-05-18 16:46:40 +0200

    [add] signin route

--- JT/api/api/index.js
+++ JT/api/api/index.js
@@ -3,6 +3,8 @@ const app = express();
 const PORT = 3000;
 const bodyParser = require('body-parser');
 const db = require('./config/db.js');
+const jwt = require('jsonwebtoken');
+const secretKey = 'a2f4b978b4e89347e906b7c16ae3d9f98d3f67d9ed9e7a2f9b5f4c8a56e3b2d4';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,26 +14,38 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/login', async (req, res) => {
+    const { token } = req.body;
+})
+
 app.post('/signin', async (req, res) => {
     const { username, password, poids, taille } = req.body;
-    res.send(password);
-    // try {
-    //     const [resultat] = await executeAsync("SELECT * FROM user WHERE username = ?", [username]);
-    //     if (resultat.length != 0) {
-    //         return res.status(409).send({ "msg": "User already exists" });
-    //     }
-    //     await executeAsync(
-    //         "INSERT INTO user (username, password, poids, taille) VALUES (?, ?, ?, ?)",
-    //         [username, password, poids, taille]
-    //     );
-    //     const token = jwt.sign({ username: username }, secretKey);
-    //     res.json({ 'token': token });
-    // } catch (err) {
-    //     res.status(500).send({ "msg": "Erreur serveur" });
-    // }
+    if (!username || !password || !poids || !taille) {
+        res.status(201).send({"msg": "Need 4 parameters"});
+    }
+    try {
+        db.execute("SELECT * FROM user WHERE username = ?", [username], function (err, resultat, fields) {
+            if (err) {
+                res.status(500).send({"msg": "Error intern"})
+            }
+            if (resultat.length >= 1) {
+                return res.status(409).send({ "msg": "User already exists" });
+            }
+            db.execute("INSERT INTO user (username, password, poids, taille) VALUES (?, ?, ?, ?)", [username, password, poids, taille], function(err, resu, fields) {
+                if (err) {
+                    res.status(500).send({"msg": "Error intern"})
+                }
+                const token = jwt.sign({ username: username }, secretKey);
+                res.json({ 'token': token });
+                res.status(200).send({"token": token});
+            });
+        });
+    } catch (err) {
+        res.status(500).send({"msg": "Error intern"})
+    }
 });
 
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
-  })
+})
